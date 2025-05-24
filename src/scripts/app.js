a3a1c918dec678f3a363f3324cda1fb2e643fb22
Sahan <sahan.toksoz@student.heaj.be>
2025-05-25 01:07:20 +0200

    Test Design Fiction
    Test de vérification fonctionnel de GitHub

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,12 +1,2 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () => {
-  const button = document.getElementById("toggle-button");
-  const content = document.getElementById("content");
-
-  button.addEventListener("click", () => {
-    content.classList.toggle("active");
-  });
-});
-  
-  
