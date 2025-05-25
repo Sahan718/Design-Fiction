a08b0cd686feb1a6e2426d55ebdefd287754f2ee
Sahan <sahan.toksoz@student.heaj.be>
2025-05-25 17:13:38 +0200

    Java + Images

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,15 @@
 "use strict";
 
+document.addEventListener("DOMContentLoaded", () => {
+  const chapters = document.querySelectorAll('[class^="Chapter"]:not(.Chapter-details)');
+
+  chapters.forEach(chapter => {
+    const nextElement = chapter.nextElementSibling;
+
+    if (nextElement && nextElement.classList.contains('Chapter-details')) {
+      chapter.addEventListener("click", () => {
+        nextElement.classList.toggle("active");
+      });
+    }
+  });
+});
